27cd5b71a721aea315c269ab6ed523cdd75e19a4
mdforidurrahman <mdforidurrahman205@gmail.com>
2023-07-11 21:05:33 +0600

    Create First Day of Python.py

---
+++ First Day of Python.py
@@ -0,0 +1,27 @@
+print("Hello, World!")
+
+#Comment
+#I love python 
+
+print("Hello, World!") #This is a comment
+
+'''
+I love python I love python I love python I love python I love python
+I love python I love python I love python I love python I love python
+I love python I love python I love python I love python I love python
+I love python I love python I love python I love python I love python
+'''
+
+
+"""
+I love python I love python I love python I love python I love python
+I love python I love python I love python I love python I love python
+I love python I love python I love python I love python I love python
+I love python I love python I love python I love python I love python
+"""
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
