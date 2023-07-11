bda0532ca2035e0588b4e88530b7ed8507745f82
mdforidurrahman <mdforidurrahman205@gmail.com>
2023-07-11 21:27:05 +0600

    Update First Day of Python.py

--- First Day of Python.py
+++ First Day of Python.py
@@ -1,9 +1,9 @@
 print("Hello, World!")
 
-#Comment
-#I love python 
+# Comment
+# I love python
 
-print("Hello, World!") #This is a comment
+print("Hello, World!")  # This is a comment
 
 '''
 I love python I love python I love python I love python I love python
@@ -21,7 +21,26 @@ I love python I love python I love python I love python I love python
 """
 
 
-#This is a comment
-#written in
-#more than just one line
+# This is a comment
+# written in
+# more than just one line
 print("Hello, World!")
+
+
+a = 'I'
+b = 'Love'
+c = 'Bangladesh'
+d = a+b+c
+print(d)
+
+a = 'I'
+b = ' Love'
+c = ' Bangladesh'
+d = a+b+c
+print(d)
+
+a = 'I'
+b = 'Love'
+c = 'Bangladesh'
+d = a + " " + b + " " + c
+print(d)
